b02f57e73c445ce6d59a5dc1683f057c74a3e17f
Martin Soto <donsoto@gmail.com>
2017-02-12 17:25:45 +0100

    Adjust docker-compose environment to run Clojure REPL.
    The REPL can be started using a "lein" script that mimics the regular
    lein command by executing lein inside a container. It mounts the
    complete user directory into the container and passes the used
    identity (user and group ids) using environment variables.

---
+++ docker/clojure/entrypoint.sh
@@ -0,0 +1,27 @@
+#! /bin/bash -e
+
+set -o pipefail
+
+setup_user() {
+    useradd_params=''
+    if [ ! -z "$USER_UID" ]; then
+        useradd_params="$useradd_params --uid $USER_UID"
+    fi
+    if [ ! -z "$USER_GID" ]; then
+        useradd_params="$useradd_params --gid $USER_GID"
+    fi
+    if [ ! -z "$USER_DIR" ]; then
+        mkdir -p $USER_DIR
+        useradd_params="$useradd_params --home-dir $USER_DIR"
+    fi
+    if [ -z "$USER_NAME" ]; then
+        USER_NAME=build
+    fi
+    useradd_params="$useradd_params $USER_NAME"
+
+    useradd $useradd_params
+}
+
+setup_user
+
+exec gosu $USER_NAME "$@"
