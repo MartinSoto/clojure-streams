32dc2d5fb06b23962038436ecebeeffa83ed52d9
Martin Soto <donsoto@gmail.com>
2017-02-12 17:39:05 +0100

    Add crude Kafka producer.

--- clstreams/project.clj
+++ clstreams/project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.kafka/kafka-clients "0.10.1.0"]]
+
   :main ^:skip-aot clstreams.core
   :target-path "target/%s"
+
   :profiles {:uberjar {:aot :all}})
--- clstreams/src/clstreams/core.clj
+++ clstreams/src/clstreams/core.clj
@@ -1,7 +1,27 @@
 (ns clstreams.core
-  (:gen-class))
+  (:gen-class)
+  (:import [org.apache.kafka.clients.producer KafkaProducer ProducerRecord]))
+
+(defn client-props []
+  (let [props (java.util.Properties.)]
+    (.put props "bootstrap.servers" "kafka:9092")
+    (.put props "acks" "all")
+    (.put props "retries" (int 0))
+    (.put props "batch.size" (int 16384))
+    (.put props "linger.ms" (int 1))
+    (.put props "buffer.memory" (int 33554432))
+    (.put props "key.serializer" "org.apache.kafka.common.serialization.StringSerializer")
+    (.put props "value.serializer" "org.apache.kafka.common.serialization.StringSerializer")
+
+    props))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [topic "my-topic"
+        props (client-props)
+        producer (KafkaProducer. props)]
+    (doseq
+        [n (range 100)]
+      (.send producer
+             (ProducerRecord. topic (format "msg%04d" n) (format "This is message %d" n))))
+    (.close producer)))
