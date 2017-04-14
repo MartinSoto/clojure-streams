809d03220e78ddaf2e769c0c4a3eaa041e69204c
Martin Soto <donsoto@gmail.com>
2017-04-14 23:33:17 +0200

    Refactor property handling in Streams example.

--- clstreams/src/clstreams/core.clj
+++ clstreams/src/clstreams/core.clj
@@ -9,24 +9,21 @@
 
 (require '[clojure.string :as str])
 
-(defn client-props []
-  (let [props (java.util.Properties.)]
-    (.put props "bootstrap.servers" "kafka:9092")
-    (.put props "acks" "all")
-    (.put props "retries" (int 0))
-    (.put props "batch.size" (int 16384))
-    (.put props "linger.ms" (int 1))
-    (.put props "buffer.memory" (int 33554432))
-    (.put props "key.serializer" "org.apache.kafka.common.serialization.StringSerializer")
-    (.put props "value.serializer" "org.apache.kafka.common.serialization.StringSerializer")
-
-    props))
+(def client-props
+  (doto (java.util.Properties.)
+    (.put "bootstrap.servers" "kafka:9092")
+    (.put "acks" "all")
+    (.put "retries" (int 0))
+    (.put "batch.size" (int 16384))
+    (.put "linger.ms" (int 1))
+    (.put "buffer.memory" (int 33554432))
+    (.put "key.serializer" "org.apache.kafka.common.serialization.StringSerializer")
+    (.put "value.serializer" "org.apache.kafka.common.serialization.StringSerializer")))
 
 (defn produce-numbered-messages
   [& args]
   (let [topic "my-topic"
-        props (client-props)
-        producer (KafkaProducer. props)]
+        producer (KafkaProducer. client-props)]
     (doseq
         [n (range 100)]
       (.send producer
@@ -36,33 +33,29 @@
 (defn produce-words
   [& args]
   (let [topic "streams-file-input"
-        props (client-props)
-        producer (KafkaProducer. props)]
+        producer (KafkaProducer. client-props)]
     (doto producer
       (.send (ProducerRecord. topic "" "all streams lead to kafka"))
       (.send (ProducerRecord. topic "" "hello kafka streams"))
       (.send (ProducerRecord. topic "" "join kafka summit"))
       .close)))
 
-(defn count-words-props []
-  (let [props (java.util.Properties.)]
-    (.put props StreamsConfig/APPLICATION_ID_CONFIG, "streams-wordcount")
-    (.put props StreamsConfig/BOOTSTRAP_SERVERS_CONFIG, "kafka:9092")
-    (.put props StreamsConfig/KEY_SERDE_CLASS_CONFIG, (-> (Serdes/String) .getClass .getName))
-    (.put props StreamsConfig/VALUE_SERDE_CLASS_CONFIG, (-> (Serdes/String) .getClass .getName))
+(def count-words-props
+  (doto (java.util.Properties.)
+    (.put StreamsConfig/APPLICATION_ID_CONFIG, "streams-wordcount")
+    (.put StreamsConfig/BOOTSTRAP_SERVERS_CONFIG, "kafka:9092")
+    (.put StreamsConfig/KEY_SERDE_CLASS_CONFIG, (-> (Serdes/String) .getClass .getName))
+    (.put StreamsConfig/VALUE_SERDE_CLASS_CONFIG, (-> (Serdes/String) .getClass .getName))
 
     ; setting offset reset to earliest so that we can re-run the demo
     ; code with the same pre-loaded data
     ; Note: To re-run the demo, you need to use the offset reset tool:
     ; https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
-    (.put props ConsumerConfig/AUTO_OFFSET_RESET_CONFIG, "earliest")
-
-    props))
+    (.put ConsumerConfig/AUTO_OFFSET_RESET_CONFIG, "earliest")))
 
 (defn count-words
   [& args]
-  (let [props (count-words-props)
-        builder (KStreamBuilder.)
+  (let [builder (KStreamBuilder.)
         source (.stream builder (into-array String ["streams-file-input"]))
         counts (-> source
                    (.flatMapValues
@@ -81,15 +74,14 @@
                    .groupByKey
                    (.count "Counts"))
         xxx (.to counts (Serdes/String) (Serdes/Long) "streams-wordcount-output")
-        streams (KafkaStreams. builder props)]
+        streams (KafkaStreams. builder count-words-props)]
 
     (.start streams)
     (Thread/sleep 5000)
     (.close streams)))
 
 (defn print-word-counts[& args]
-  (let [props (count-words-props)
-        builder (KStreamBuilder.)
+  (let [builder (KStreamBuilder.)
         source (.stream builder (Serdes/String) (Serdes/Long)
                         (into-array String ["streams-wordcount-output"]))
         print-counts (-> source
@@ -98,7 +90,7 @@
                             (apply [this key value]
                               (println key value)
                               ))))
-        streams (KafkaStreams. builder props)]
+        streams (KafkaStreams. builder count-words-props)]
 
     (.start streams)
     (Thread/sleep 5000)
