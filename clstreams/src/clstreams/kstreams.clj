92b53c34148170e42bcff6264ab9043e5d46830f
Martin Soto <donsoto@gmail.com>
2017-04-16 23:51:11 +0200

    Implement macro for instantiating Java functional interfaces.

--- clstreams/src/clstreams/kstreams.clj
+++ clstreams/src/clstreams/kstreams.clj
@@ -1,2 +1,11 @@
-(ns clstreams.kstreams)
+(ns clstreams.kstreams
+  (:require [clojure.reflect]))
 
+(defmacro java-function
+  [iface-symbol impl-fn-expr]
+  (let [iface-refl (clojure.reflect/reflect (eval iface-symbol))
+        method-refl (first (:members iface-refl))
+        param-syms (map (fn [_] (gensym "p")) (:parameter-types method-refl))]
+    `(reify ~iface-symbol
+       (~(:name method-refl) [_# ~@param-syms]
+        (~impl-fn-expr ~@param-syms)))))
--- clstreams/test/clstreams/kstreams_test.clj
+++ clstreams/test/clstreams/kstreams_test.clj
@@ -1,23 +1,19 @@
 (ns clstreams.kstreams-test
   (:require [clojure.test :refer :all]
             [clstreams.kstreams :refer :all])
-  (:import [org.apache.kafka.streams.kstream KeyValueMapper]))
-
-(def kvmapper-reflection {:bases nil,
-                          :flags #{:interface :public :abstract},
-                          :members
-                          #{{:name "apply",
-                             :return-type java.lang.Object,
-                             :declaring-class KeyValueMapper,
-                             :parameter-types [java.lang.Object java.lang.Object],
-                             :exception-types [],
-                             :flags #{:public :abstract}}}})
+  (:import [org.apache.kafka.streams.kstream Aggregator KeyValueMapper Initializer]))
 
 (deftest test-java-function
   (testing "Can build a functional interface from an inline function"
-    (let [kvm (java-function kvmapper-reflection (fn [key value] [(+ 2 key) (* 3 value)]))]
+    (let [kvm (java-function KeyValueMapper (fn [key value] [(+ 2 key) (* 3 value)]))]
       (is (= (.apply kvm 1 2) [3 6]))))
   (testing "Can build a functional interface from a predefined function"
     (let [mapper (fn [key value] [(- 5 key) (* 2 value)])
-          kvm (java-function kvmapper-reflection mapper)]
-      (is (= (.apply kvm 1 2) [4 4])))))
+          kvm (java-function KeyValueMapper mapper)]
+      (is (= (.apply kvm 1 2) [4 4]))))
+  (testing "Can build a functional interface for three parameters"
+    (let [agrt (java-function Aggregator (fn [key value aggr] [key (+ aggr value)]))]
+      (is (= (.apply agrt 1 4 5) [1 9]))))
+  (testing "Can build a functional interface for no parameters"
+    (let [init (java-function Initializer (fn [] 25))]
+      (is (= (.apply init) 25)))))
