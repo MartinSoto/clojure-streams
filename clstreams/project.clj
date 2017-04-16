6202e3d863ea0142dfef3c025fc9ff26b8964c43
Martin Soto <donsoto@gmail.com>
2017-04-16 23:52:19 +0200

    Add tools.namespace to the dependencies.

--- clstreams/project.clj
+++ clstreams/project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.namespace "0.3.0-alpha3"]
+
                  [org.apache.kafka/kafka-clients "0.10.1.0"]
                  [org.apache.kafka/kafka-streams "0.10.1.0"]]
 
