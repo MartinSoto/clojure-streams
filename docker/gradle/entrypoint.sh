5251cd8f256a72d51eaf27b3f1b350ea32289a84
Martin Soto <donsoto@gmail.com>
2017-04-22 00:36:12 +0200

    Store Gradle state in a Docker volume
    Also reorganize the entry point script following the lines of the one
    for Clojure.

--- docker/gradle/entrypoint.sh
+++ docker/gradle/entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -o pipefail
 
+if [ -z "$USER_NAME" ]; then
+    USER_NAME=build
+fi
+
+home_base=/container_home
+
 setup_user() {
     useradd_params=''
     if [ ! -z "$USER_UID" ]; then
@@ -10,18 +16,34 @@ setup_user() {
     if [ ! -z "$USER_GID" ]; then
         useradd_params="$useradd_params --gid $USER_GID"
     fi
-    if [ ! -z "$USER_DIR" ]; then
-        mkdir -p $USER_DIR
-        useradd_params="$useradd_params --home-dir $USER_DIR"
-    fi
-    if [ -z "$USER_NAME" ]; then
-        USER_NAME=build
-    fi
-    useradd_params="$useradd_params $USER_NAME"
+    useradd_params="$useradd_params -m --home-dir $home_base/$USER_NAME $USER_NAME"
 
+    mkdir -p $home_base
     useradd $useradd_params
 }
 
+setup_maven_repo() {
+    maven_dir=/var/lib/maven
+    maven_repo=$maven_dir/m2
+
+    mkdir -p $maven_repo
+    chown $USER_NAME: $maven_repo
+
+    ln -s $maven_repo $home_base/$USER_NAME/.m2
+}
+
+setup_gradle_dir() {
+    gradle_dir=/var/lib/gradle
+    gradle_data=$gradle_dir/data
+
+    mkdir -p $gradle_data
+    chown $USER_NAME: $gradle_data
+
+    ln -s $gradle_data $home_base/$USER_NAME/.gradle
+}
+
 setup_user
+setup_maven_repo
+setup_gradle_dir
 
 exec gosu $USER_NAME "$@"
