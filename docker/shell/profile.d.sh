6bfbcbacc7fb2d2d7f1c1e09118f791ba5be766e
Martin Soto <donsoto@gmail.com>
2017-04-22 00:59:31 +0200

    Save shell history on main project directory

--- docker/shell/profile.d.sh
+++ docker/shell/profile.d.sh
@@ -1,4 +1,9 @@
 # Add bin directory in project path to global PATH.
 if [ ! -z "$PROJECT_DIR" ]; then
     PATH=$PROJECT_DIR/bin:$PATH
+
+    HISTFILE=$PROJECT_DIR/.bash_history
+    HISTFILESIZE=1000
+    HISTSIZE=1000
+    HISTCONTROL=ignoredups
 fi
