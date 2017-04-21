f1ddd0906f52cca110db7f25a12537c3b514a312
Martin Soto <donsoto@gmail.com>
2017-04-22 00:09:25 +0200

    Clean-up entry point for shell container

--- docker/shell/entrypoint.sh
+++ docker/shell/entrypoint.sh
@@ -2,9 +2,13 @@
 
 set -o pipefail
 
-setup_user() {
-    home_base=/container_home
+if [ -z "$USER_NAME" ]; then
+    USER_NAME=build
+fi
 
+home_base=/container_home
+
+setup_user() {
     useradd_params=''
     if [ ! -z "$USER_UID" ]; then
         useradd_params="$useradd_params --uid $USER_UID"
@@ -12,21 +16,18 @@ setup_user() {
     if [ ! -z "$USER_GID" ]; then
         useradd_params="$useradd_params --gid $USER_GID"
     fi
-    if [ ! -z "$USER_DIR" ]; then
-        mkdir -p $USER_DIR
-    fi
-    if [ -z "$USER_NAME" ]; then
-        USER_NAME=build
-    fi
     useradd_params="$useradd_params -m --home-dir $home_base/$USER_NAME $USER_NAME"
 
     mkdir -p $home_base
     useradd $useradd_params
+}
 
+setup_docker_access() {
     docker_group=$(stat -c '%G' /var/run/docker.sock)
     usermod -aG $docker_group $USER_NAME
 }
 
 setup_user
+setup_docker_access
 
 exec gosu $USER_NAME "$@"
