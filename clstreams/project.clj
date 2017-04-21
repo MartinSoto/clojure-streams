aec1b3182d03dd371fc91b4c518091a6f49ba292
Martin Soto <donsoto@gmail.com>
2017-04-22 00:39:47 +0200

    Add test for the word-count topology.
    Because of https://issues.apache.org/jira/browse/KAFKA-4461, this test
    doesn't run with the current Kafka release. For the time being, the
    dependencies will point to an unreleased Kafka version.

--- clstreams/project.clj
+++ clstreams/project.clj
@@ -7,10 +7,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.namespace "0.3.0-alpha3"]
 
-                 [org.apache.kafka/kafka-clients "0.10.2.0"]
-                 [org.apache.kafka/kafka-clients "0.10.2.0" :classifier "test"]
-                 [org.apache.kafka/kafka-streams "0.10.2.0"]
-                 [org.apache.kafka/kafka-streams "0.10.2.0" :classifier "test"]]
+                 [org.apache.kafka/kafka-clients "0.10.2.1"]
+                 [org.apache.kafka/kafka-clients "0.10.2.1" :classifier "test"]
+                 [org.apache.kafka/kafka-streams "0.10.2.1"]
+                 [org.apache.kafka/kafka-streams "0.10.2.1" :classifier "test"]]
 
   :main ^:skip-aot clstreams.core
   :target-path "target/%s"
--- clstreams/test/clstreams/core_test.clj
+++ clstreams/test/clstreams/core_test.clj
@@ -8,7 +8,7 @@
 (def string-serializer (StringSerializer.))
 (def string-deserializer (StringDeserializer.))
 
-(deftest test-word-count-topology
+(deftest test-copy-topology
   (testing "It works"
     (let [builder (KStreamBuilder.)
           _ (build-copy builder)
@@ -20,3 +20,19 @@
                                 string-deserializer string-deserializer)]
         (is (= (.key record) ""))
         (is (= (.value record) "all streams lead to kafka"))))))
+
+(deftest test-word-count-topology
+  (testing "It works"
+    (let [builder (KStreamBuilder.)
+          _ (build-count-words builder)
+          driver (ProcessorTopologyTestDriver. count-words-props builder
+                                               (into-array String ["Counts"]))]
+      (.process driver "streams-file-input" "" "all streams lead to kafka"
+                string-serializer string-serializer)
+      (.process driver "streams-file-input" "" "hello kafka streams"
+                string-serializer string-serializer)
+      (.process driver "streams-file-input" "" "join kafka summit"
+                string-serializer string-serializer)
+      (let [record (.readOutput driver "streams-wordcount-output")]
+        (println (-> driver (.getKeyValueStore "Counts") .all iterator-seq))
+        (is (some? record))))))
