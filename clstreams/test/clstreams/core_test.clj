2daacf770a7b42412d8fd1335fe6dccb4164dbfe
Martin Soto <donsoto@gmail.com>
2017-04-22 15:25:16 +0200

    Make the count-words test really test the output.

--- clstreams/test/clstreams/core_test.clj
+++ clstreams/test/clstreams/core_test.clj
@@ -2,14 +2,22 @@
   (:require [clojure.test :refer :all]
             [clstreams.core :refer :all])
   (:import [org.apache.kafka.test ProcessorTopologyTestDriver]
-           [org.apache.kafka.common.serialization StringSerializer StringDeserializer]
+           [org.apache.kafka.common.serialization Serdes]
            [org.apache.kafka.streams.kstream KStreamBuilder]))
 
-(def string-serializer (StringSerializer.))
-(def string-deserializer (StringDeserializer.))
+(def string-serializer (-> (Serdes/String) .serializer))
+(def string-deserializer (-> (Serdes/String) .deserializer))
+
+(def long-deserializer (-> (Serdes/Long) .deserializer))
+
+(defn key-value-map [key-value-objs]
+  (into {} (map #(vector (.key %) (.value %)) key-value-objs)))
+
+(defn collect-output [record-producing-fn]
+  (->> record-producing-fn repeatedly (take-while identity) key-value-map))
 
 (deftest test-copy-topology
-  (testing "It works"
+  (testing "Garbage in, garbage out..."
     (let [builder (KStreamBuilder.)
           _ (build-copy builder)
           driver (ProcessorTopologyTestDriver. count-words-props builder
@@ -22,7 +30,7 @@
         (is (= (.value record) "all streams lead to kafka"))))))
 
 (deftest test-word-count-topology
-  (testing "It works"
+  (testing "Complete table written to output topic"
     (let [builder (KStreamBuilder.)
           _ (build-count-words builder)
           driver (ProcessorTopologyTestDriver. count-words-props builder
@@ -33,6 +41,10 @@
                 string-serializer string-serializer)
       (.process driver "streams-file-input" "" "join kafka summit"
                 string-serializer string-serializer)
-      (let [record (.readOutput driver "streams-wordcount-output")]
-        (println (-> driver (.getKeyValueStore "Counts") .all iterator-seq))
-        (is (some? record))))))
+      (let [output (collect-output #(.readOutput driver "streams-wordcount-output"
+                                                 string-deserializer long-deserializer))]
+        (let [expected {"all" 1, "streams" 2, "lead" 1, "to" 1,
+                        "kafka" 3, "hello" 1, "join" 1, "summit" 1}]
+          (is (= output expected) "Output topic contents are correct")
+          (is (= (-> driver (.getKeyValueStore "Counts") .all iterator-seq key-value-map)
+                 expected) "Store contents are correct"))))))
