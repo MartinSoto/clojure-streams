0fa6484f3a347f633855132bdd16ec605523290e
Martin Soto <donsoto@gmail.com>
2017-05-09 01:47:38 +0200

    Refactor component test to use new spy implementation

--- clstreams/test/clstreams/webapi/component_test.clj
+++ clstreams/test/clstreams/webapi/component_test.clj
@@ -1,37 +1,37 @@
 (ns clstreams.webapi.component-test
   (:require [clojure.test :refer :all]
+            [clstreams.testutil.spies :refer [spy with-spy-redefs calls]]
             [clstreams.webapi.component :refer :all]
             [com.stuartsierra.component :as component]
-            [conjure.core :as conjure]
             [immutant.web :as web]))
 
 (def app-factory nil)
 
 (deftest test-immutant-component
   (testing "Component starts and stops server"
-    (let [handler :ze-handler
-          server-dummy {:server "ze-server"}]
-      (conjure/stubbing
-       [web/run server-dummy
-        web/stop nil
-        app-factory handler]
+    (let [handler (fn [& x] nil)
+          server-dummy {:server "ze-server"}
+          [app-factory app-factory-spy] (spy handler)]
+      (with-spy-redefs
+        [[web/run web-run-spy] (spy server-dummy)
+         [web/stop web-stop-spy] (spy)]
 
-       (let [im (new-immutant app-factory (sorted-map :host "0.1.2.3" :port 1234))]
-         (is (= (:server im) nil))
-         (conjure/verify-call-times-for web/run 0)
-         (conjure/verify-call-times-for web/stop 0)
+        (let [im (new-immutant app-factory (sorted-map :host "0.1.2.3" :port 1234))]
+          (is (= (:server im) nil))
+          (is (= (-> web-run-spy calls count) 0))
+          (is (= (-> web-stop-spy calls count) 0))
 
-         (let [started-im (component/start im)]
-           (is (= (:server started-im) server-dummy))
-           (conjure/verify-call-times-for web/run 1)
-           (conjure/verify-call-times-for web/stop 0)
-           (conjure/verify-call-times-for app-factory 1)
-           (conjure/verify-first-call-args-for app-factory im)
+          (let [started-im (component/start im)]
+            (is (= (:server started-im) server-dummy))
+            (is (= (-> web-run-spy calls count) 1))
+            (is (= (-> web-stop-spy calls count) 0))
+            (is (= (-> app-factory-spy calls count) 1))
+            (is (= (-> app-factory-spy calls first) [im]))
 
-           (let [stopped-im (component/stop started-im)]
-             (is (= (:server stopped-im) nil)))))
+            (let [stopped-im (component/stop started-im)]
+              (is (= (:server stopped-im) nil)))))
 
-       (conjure/verify-call-times-for web/run 1)
-       (conjure/verify-first-call-args-for web/run handler :host "0.1.2.3":port 1234)
-       (conjure/verify-call-times-for web/stop 1)
-       (conjure/verify-first-call-args-for web/stop server-dummy)))))
+        (is (= (-> web-run-spy calls count) 1))
+        (is (= (-> web-run-spy calls first) [handler :host "0.1.2.3":port 1234]))
+        (is (= (-> web-stop-spy calls count) 1))
+        (is (= (-> web-stop-spy calls first) [server-dummy]))))))
