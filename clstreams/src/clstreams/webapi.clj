38480e9dfba90a3f634c4874631ebfa46d836f2f
Martin Soto <donsoto@gmail.com>
2017-05-12 16:02:38 +0200

    Handle web requests asynchronously using Aleph and core.async

--- clstreams/src/clstreams/webapi.clj
+++ clstreams/src/clstreams/webapi.clj
@@ -1,6 +1,8 @@
 (ns clstreams.webapi
   (:require [bidi.ring :as bidi]
+            [clojure.core.async :refer [<! go timeout]]
             [clstreams.webapi.component :refer [new-aleph]]
+            [manifold.stream :as stream]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [ring.util.response :refer [response]])
   (:import org.apache.kafka.streams.state.QueryableStoreTypes))
@@ -15,8 +17,17 @@
   (bidi/make-handler
    [["/word-count/" :word] (make-word-count-handler component)]))
 
+(defn make-async-handler [component]
+  (fn [request]
+    (let [handler (make-main-handler component)]
+      (-> (go
+            (<! (timeout 900))
+            (handler request))
+          stream/->source
+          stream/take!))))
+
 (defn make-app [component]
-  (-> (make-main-handler component)
+  (-> (make-async-handler component)
       wrap-json-body
       wrap-json-response))
 
