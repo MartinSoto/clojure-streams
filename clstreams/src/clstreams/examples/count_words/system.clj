a79c17ce630d128582633d7c9edd29ae9b09262a
Martin Soto <donsoto@gmail.com>
2017-05-14 00:02:57 +0200

    Add a producer and a printer to the count-words example

--- clstreams/src/clstreams/examples/count_words/system.clj
+++ clstreams/src/clstreams/examples/count_words/system.clj
@@ -1,11 +1,21 @@
 (ns clstreams.examples.count-words.system
   (:require [clstreams.examples.count-words.pipelines :as pipelines]
             [clstreams.examples.count-words.webapi :as webapi]
-            [com.stuartsierra.component :as component]))
+            [clstreams.kstreams.component :refer [producer-send!]]
+            [clstreams.kstreams.helpers :as helpers]
+            [com.stuartsierra.component :as component])
+  (:import org.apache.kafka.common.serialization.Serdes))
 
 (defn count-words-system []
   (component/system-map
+   :producer (helpers/new-manual-producer "streams-file-input")
    :pipeline (pipelines/count-words)
+   :printer (helpers/new-print-topic "streams-wordcount-output"
+                                     {:key-serde (Serdes/String)
+                                      :value-serde (Serdes/Long)})
    :webapi (component/using
             (webapi/web-test)
             [:pipeline])))
+
+(defn produce-words [system & lines]
+  (apply producer-send! (:producer system) (for [line lines] ["" line])))
