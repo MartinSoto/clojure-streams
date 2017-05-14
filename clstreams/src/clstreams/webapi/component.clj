d5321435921ee806602b91495b4f748fd6dd74cc
Martin Soto <donsoto@gmail.com>
2017-05-14 18:19:22 +0200

    Make web component idempotent

--- clstreams/src/clstreams/webapi/component.clj
+++ clstreams/src/clstreams/webapi/component.clj
@@ -7,15 +7,19 @@
   component/Lifecycle
 
   (start [component]
-    (log/info "Starting web server")
-    (let [handler (app-factory component)
-          server (aleph/start-server handler config)]
-      (assoc component :server server)))
+    (if server
+      component
+      (do (log/info "Starting web server")
+          (let [handler (app-factory component)
+                server (aleph/start-server handler config)]
+            (assoc component :server server)))))
 
   (stop [component]
-    (log/info "Stopping web server")
-    (.close (:server component))
-    (assoc component :server nil)))
+    (if server
+      (do (log/info "Stopping web server")
+          (.close server)
+          (assoc component :server nil))
+      component)))
 
 (defn new-aleph [app-factory config]
   (map->AlephWebServer {:app-factory app-factory :config config}))
