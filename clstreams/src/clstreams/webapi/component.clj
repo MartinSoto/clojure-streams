25a8bba0c716a1dfd3478cb734089c68cc6d30a2
Martin Soto <donsoto@gmail.com>
2017-05-14 18:20:37 +0200

    Remove (already commented-out) Immutant component

--- clstreams/src/clstreams/webapi/component.clj
+++ clstreams/src/clstreams/webapi/component.clj
@@ -24,20 +24,3 @@
 (defn new-aleph [app-factory config]
   (map->AlephWebServer {:app-factory app-factory :config config}))
 
-
-(comment defrecord ImmutantWebServer [app-factory config server]
-  component/Lifecycle
-
-  (start [component]
-    (log/info "Starting web server")
-    (let [handler (app-factory component)
-          server (apply web/run handler (mapcat seq config))]
-      (assoc component :server server)))
-
-  (stop [component]
-    (log/info "Stopping web server")
-    (web/stop server)
-    (assoc component :server nil)))
-
-(comment defn new-immutant [app-factory config]
-  (map->ImmutantWebServer {:app-factory app-factory :config config}))
