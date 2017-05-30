df5747675827b336a7afd4133ecc33d73466d2bc
Martin Soto <donsoto@gmail.com>
2017-05-30 22:08:55 +0200

    Add verify functions to Mockito wrapper

--- clstreams/test/clstreams/testutil/mockito.clj
+++ clstreams/test/clstreams/testutil/mockito.clj
@@ -14,3 +14,21 @@
 
 (defmacro on-call [mock-fn-expr parameter-expr-list return-value-expr]
   `(return-> ~mock-fn-expr (.invoke ~@parameter-expr-list) ~return-value-expr))
+
+(defmacro verify->
+  ([mock-obj-expr method-invocation-form]
+   `(verify-> ~mock-obj-expr ~method-invocation-form (Mockito/times 1)))
+  ([mock-obj-expr method-invocation-form counting-predicate-expr]
+   `(-> ~mock-obj-expr (Mockito/verify ~counting-predicate-expr) ~method-invocation-form)))
+
+(defmacro verify-fn
+  ([mock-fn-expr parameter-expr-list]
+   `(verify-> ~mock-fn-expr (.invoke ~@parameter-expr-list)))
+  ([mock-fn-expr parameter-expr-list counting-predicate-expr]
+   `(verify-> ~mock-fn-expr (.invoke ~@parameter-expr-list) ~counting-predicate-expr)))
+
+(defn times [n] (Mockito/times n))
+(defn at-least-once [] (Mockito/atLeastOnce))
+(defn at-least [n] (Mockito/atLeast n))
+(defn at-most [n] (Mockito/atMost n))
+(defn never [] (Mockito/never))
