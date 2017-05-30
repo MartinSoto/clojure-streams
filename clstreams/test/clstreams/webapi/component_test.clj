ac375cce7d7cdd0c12a8ac80740e4f7b5b6cbd93
Martin Soto <donsoto@gmail.com>
2017-05-30 22:09:14 +0200

    Convert web component test to using Mockito

--- clstreams/test/clstreams/webapi/component_test.clj
+++ clstreams/test/clstreams/webapi/component_test.clj
@@ -1,67 +1,40 @@
 (ns clstreams.webapi.component-test
   (:require [aleph.http :as aleph]
             [clojure.test :refer :all]
-            [clstreams.testutil.spies :refer [spy with-spy-redefs calls]]
-            [clstreams.webapi.component :refer :all]
-            [com.stuartsierra.component :as component]))
+            [clstreams.testutil.component :as tu-comp]
+            [clstreams.testutil.mockito :as mkto]
+            [clstreams.webapi.component :refer :all])
+  (:import org.mockito.Mockito))
 
 (def app-factory nil)
 
 (deftest test-aleph-component
   (testing "Component starts and stops server"
-    (let [handler (fn [& x] nil)
-          [stop-server stop-server-spy] (spy)
-          server-dummy (reify java.io.Closeable
-                         (close [this] (stop-server this)))
-          [app-factory app-factory-spy] (spy handler)]
-      (with-spy-redefs
-        [[aleph/start-server start-server-spy] (spy server-dummy)]
+    (let [config {:host "0.1.2.3" :port 1234}
 
-        (let [aleph (new-aleph app-factory {:host "0.1.2.3" :port 1234})]
-          (is (= (:server aleph) nil))
-          (is (= (-> start-server-spy calls count) 0))
-          (is (= (-> stop-server-spy calls count) 0))
+          handler (mkto/mock-fn)
+          app-factory (mkto/on-call (mkto/mock-fn) [(Mockito/any)] handler)
 
-          (let [started-aleph (component/start aleph)]
-            (is (= (:server started-aleph) server-dummy))
-            (is (= (-> start-server-spy calls count) 1))
-            (is (= (-> stop-server-spy calls count) 0))
-            (is (= (-> app-factory-spy calls count) 1))
-            (is (= (-> app-factory-spy calls first) [aleph]))
+          server-mock (mkto/mock java.io.Closeable)
+          start-server-fn (mkto/on-call (mkto/mock-fn) [handler config] server-mock)]
 
-            (let [stopped-aleph (component/stop started-aleph)]
-              (is (= (:stop-server stopped-aleph) nil)))))
+      (with-redefs [aleph/start-server start-server-fn]
 
-        (is (= (-> start-server-spy calls count) 1))
-        (is (= (-> start-server-spy calls first) [handler {:host "0.1.2.3" :port 1234}]))
-        (is (= (-> stop-server-spy calls count) 1))
-        (is (= (-> stop-server-spy calls first) [server-dummy]))))))
+        (tu-comp/cycle-component
 
-(comment deftest test-immutant-component
-  (testing "Component starts and stops server"
-    (let [handler (fn [& x] nil)
-          server-dummy {:server "ze-server"}
-          [app-factory app-factory-spy] (spy handler)]
-      (with-spy-redefs
-        [[web/run web-run-spy] (spy server-dummy)
-         [web/stop web-stop-spy] (spy)]
+         [aleph (new-aleph app-factory config)]
+         ((is (= (:server aleph) nil))
+          (mkto/verify-fn start-server-fn [handler config] (mkto/never))
+          (mkto/verify-> server-mock (.close) (mkto/never)))
 
-        (let [im (new-immutant app-factory (sorted-map :host "0.1.2.3" :port 1234))]
-          (is (= (:server im) nil))
-          (is (= (-> web-run-spy calls count) 0))
-          (is (= (-> web-stop-spy calls count) 0))
+         [started-aleph]
+         ((is (= (:server started-aleph) server-mock))
+          (mkto/verify-fn start-server-fn [handler config])
+          (mkto/verify-> server-mock (.close) (mkto/never))
+          (mkto/verify-fn app-factory [aleph]))
 
-          (let [started-im (component/start im)]
-            (is (= (:server started-im) server-dummy))
-            (is (= (-> web-run-spy calls count) 1))
-            (is (= (-> web-stop-spy calls count) 0))
-            (is (= (-> app-factory-spy calls count) 1))
-            (is (= (-> app-factory-spy calls first) [im]))
+         [stopped-aleph]
+         ((is (= (:stop-server stopped-aleph) nil))
+          (mkto/verify-fn start-server-fn [handler config])
+          (mkto/verify-> server-mock (.close))))))))
 
-            (let [stopped-im (component/stop started-im)]
-              (is (= (:server stopped-im) nil)))))
-
-        (is (= (-> web-run-spy calls count) 1))
-        (is (= (-> web-run-spy calls first) [handler :host "0.1.2.3":port 1234]))
-        (is (= (-> web-stop-spy calls count) 1))
-        (is (= (-> web-stop-spy calls first) [server-dummy]))))))
