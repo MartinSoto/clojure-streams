d0a40ec87348fbba741772685cc55e455272c9c5
Martin Soto <donsoto@gmail.com>
2017-05-30 22:45:50 +0200

    Add idempotence test for Aleph component

--- clstreams/test/clstreams/webapi/component_test.clj
+++ clstreams/test/clstreams/webapi/component_test.clj
@@ -8,7 +8,7 @@
 
 (def app-factory nil)
 
-(deftest test-aleph-component
+(deftest test-cycle-aleph-component
   (testing "Component starts and stops server"
     (let [config {:host "0.1.2.3" :port 1234}
 
@@ -38,3 +38,15 @@
           (mkto/verify-fn start-server-fn [handler config])
           (mkto/verify-> server-mock (.close))))))))
 
+(deftest test-idempotent-aleph-component
+  (testing "Component starts and stops server"
+    (let [config {:host "0.1.2.3" :port 1234}
+
+          handler (mkto/mock-fn)
+          app-factory (mkto/on-call (mkto/mock-fn) [(Mockito/any)] handler)
+
+          server-mock (mkto/mock java.io.Closeable)
+          start-server-fn (mkto/on-call (mkto/mock-fn) [handler config] server-mock)]
+
+      (with-redefs [aleph/start-server start-server-fn]
+        (tu-comp/check-idempotence (new-aleph app-factory config))))))
