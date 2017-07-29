f7ae8c5ee61a81fbed37f519422e32713144bc00
Martin Soto <donsoto@gmail.com>
2017-07-29 16:06:23 +0200

    Add landscape and topology definitions to the count-words example
    They're just being validated right now, no code works on them yet.

--- clstreams/src/clstreams/examples/count_words/pipelines.clj
+++ clstreams/src/clstreams/examples/count_words/pipelines.clj
@@ -2,7 +2,10 @@
   (:require [clojure.string :as str]
             [clstreams.kafka.component :refer [new-topology]]
             [clstreams.kstreams :as ks]
-            [clstreams.kstreams.helpers :refer [default-topology-config]])
+            [clstreams.kstreams.helpers :refer [default-topology-config]]
+            [clstreams.processor :as prc]
+            [clstreams.landscape :as lsc]
+            [clojure.spec.alpha :as s])
   (:import org.apache.kafka.common.serialization.Serdes
            [org.apache.kafka.streams KeyValue StreamsConfig]
            org.apache.kafka.streams.kstream.KStreamBuilder))
@@ -23,3 +26,51 @@
 
 (defn count-words []
   (new-topology count-words-props (build-count-words)))
+
+
+(s/check-asserts true)
+
+(def count-words-landscape
+  {::lsc/streams
+   {::text-input {::lsc/topic-name "file-input"
+                  ::lsc/type ::lsc/stream
+                  ::lsc/values {::lsc/serde (Serdes/String)}}
+    ::word-counts {::lsc/topic-name "wordcount-output"
+                   ::lsc/type ::lsc/table
+                   ::lsc/keys {::lsc/serde (Serdes/String)}
+                   ::lsc/values {::lsc/serde (Serdes/Long)}}}})
+
+(s/assert ::lsc/landscape count-words-landscape)
+
+;; (def count-words-processor
+;;   [{::st/op ::st/from
+;;     ::st/topic :file-input}
+;;    {::st/op ::st/flat-map-values
+;;     ::st/fn #(-> % str/lower-case (str/split #" +"))}
+;;    {::st/op ::st/map
+;;     ::st/fn #(KeyValue. %2 %2)}
+;;    {::st/op ::st/group-by-key}
+;;    {::st/op ::st/count
+;;     ::st/store "Counts"}
+;;    {::st/op ::st/to
+;;     ::st/topic :word-counts}])
+
+(def count-words-topology
+  {:op01 {::prc/op ::prc/from
+          ::prc/topic :file-input}
+   :op02 {::prc/op ::prc/flat-map-values
+          ::prc/src :op01
+          ::prc/fn #(-> % str/lower-case (str/split #" +"))}
+   :op03 {::prc/op ::prc/map
+          ::prc/src :op02
+          ::prc/fn #(KeyValue. %2 %2)}
+   :op04 {::prc/op ::prc/group-by-key
+          ::prc/src :op03}
+   :op05 {::prc/op ::prc/count
+          ::prc/src :op04
+          ::prc/store "Counts"}
+   :op06 {::prc/op ::prc/to
+          ::prc/src :op05
+          ::prc/topic :word-counts}})
+
+(s/assert ::prc/topology count-words-topology)
